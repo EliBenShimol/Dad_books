37947cf42bde56c84f8fb94cdb1e9c68f68a14cc
EliBenShimol <elibenshimol6@gmail.com>
2025-06-18 13:24:10 +0300

    fixed margins and style

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -8,22 +9,20 @@ def read_file(filename):
             return f.read()
     except FileNotFoundError:
         return "תוכן לא נמצא"
-
+    
+    
 @app.route('/')
-def home():
+def index():
     title = read_file("text_files/title.txt")
     intro = read_file("text_files/intro.txt")
     return render_template('index.html', title=title, intro=intro)
 
-@app.route('/summary')
-def summary():
-    summary_text = read_file("text_files/summary.txt")
-    return render_template('summary.html', summary=summary_text)
-
 @app.route('/donate')
 def donate():
-    donate_text = read_file("text_files/donate.txt")
-    return render_template('donate.html', donate=donate_text)
+    donate = read_file("text_files/donate.txt")
+    return render_template('donate.html', donate=donate)
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+@app.route('/summary')
+def summary():
+    summary = read_file("text_files/summary.txt")
+    return render_template('summary.html', summary=summary)
